8e3721b6aadf5d2d10d4dc3d63d1df7f4f7945e4
hasini-herath <hasiniherath1022@gmail.com>
2023-07-26 10:18:22 +0530

    ii

--- models/room.js
+++ models/room.js
@@ -1,6 +1,6 @@
 import { Schema, model, models } from 'mongoose';
 
-const RoomSchema = new mongoose.Schema({
+const RoomSchema = new Schema({
   creator: {
     type: Schema.Types.ObjectId,
     ref: 'User',
@@ -106,6 +106,6 @@ const RoomSchema = new mongoose.Schema({
   },
 });
 
-const Room = mongoose.model('Room', RoomSchema);
+const Room = models.Room || model('Room', RoomSchema);
 
-module.exports = Room;
+export default Room;
